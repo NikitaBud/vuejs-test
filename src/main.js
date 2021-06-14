a53ceed8690419ebdc19cf3d2e952dba6b448d5a
Nikita Budiakov <budyakov91@gmail.com>
2021-06-14 08:40:32 +0300

    Added filters for text

--- src/main.js
+++ src/main.js
@@ -7,6 +7,14 @@ import './assets/sass/index.scss';
 
 Vue.config.productionTip = false;
 
+Vue.filter('dateTransform', (value) => {
+  return new Date(Date.parse(value)).toLocaleDateString();
+});
+
+Vue.filter('rankNumber', (value) => {
+  return value.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+});
+
 new Vue({
   store,
   render: (h) => h(App),
